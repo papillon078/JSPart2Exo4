a844e9f6356178ca24c325bd278d83415c90d327
papillon078 <ldc.morin@gmail.com>
2019-10-22 16:15:12 +0200

    exo4

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function erase(){
+//let lastname = document.getElementById('lastname');
+//let firstname = document.getElementById('firstname');
+//let city =  document.getElementById('city');
+document.getElementById("lastname").value = "";
+document.getElementById("firstname").value = "";
+document.getElementById("city").value = "";
+}
